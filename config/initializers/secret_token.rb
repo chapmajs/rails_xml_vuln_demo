79e452a858a8bdf49c2d42d997785b113a319937
Jonathan C <info@enablelabs.com>
2013-01-23 16:12:27 -0500

    `rails new .`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsVulnDemo::Application.config.secret_token = '8825fc2025a26924dde933f45891fcda7ec8a0d2cde8ca33d6b241279804d2c4f78391bd0dba566d552eb88285435701e25dfc500167edaa07f98a522f920682'
